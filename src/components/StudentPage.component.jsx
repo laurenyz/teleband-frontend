364f598c21eabdef5fa1c34e67c16f06ebeafef2
laurenyz <laurenkyu@gmail.com>
2020-07-24 16:49:19 -0400

    fixed merge conflicts

--- src/components/StudentPage.component.jsx
+++ src/components/StudentPage.component.jsx
@@ -2,13 +2,6 @@ import React, {useEffect, useState} from 'react'
 import StudentAssignCont from '../containers/StudentAssignCont.component'
 
 function StudentPage () {
-<<<<<<< HEAD
-    // useEffect(() => {
-    //    fetch('http://localhost:3000/students/6')
-    //    .then(resp => resp.json())
-    //    .then(student => console.log(student)) 
-    // })
-=======
 
     const [name, setName] = useState('');
     const [studentId, setStudentId] = useState('');
@@ -25,23 +18,3 @@ function StudentPage () {
     }, [])
 
 
->>>>>>> 4318c5d9b0fa4838bf425bdfdb729c8c2663922b
-    return(
-        <div>
-
-            <h3 id='student-id'>
-                Student Id: {studentId}
-            </h3>
-            <h3 id='student-name'>
-                Name: {name}
-            </h3>
-
-            <div>
-                {assignments.map(assign => <StudentAssignCont key={assign.id} assign={assign}/>)}
-            </div>
-
-        </div>
-    )
-}
-
-export default StudentPage
