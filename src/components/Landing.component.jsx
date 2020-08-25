cf7a872705852091afb6c50800a2e89f5c95409e
laurenyz <laurenkyu@gmail.com>
2020-08-24 23:40:04 -0400

    updated welcome page and login page formatting

--- src/components/Landing.component.jsx
+++ src/components/Landing.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import '../style/LandingPage.style.css'
 
 function Landing () {
     return(
-        <div>
-            Welcome to Teleband!
-
-            Click <Link to="/login">here</Link> to login.
+        <div id="welcome-message">
+            <Typography id="header-h1" variant="h1">Welcome to Teleband!</Typography>
+            <Typography display="inline" variant="h2">Click </Typography><Link to="/login"><Typography display="inline" variant="h2">here</Typography></Link><Typography display="inline" variant="h2"> to login.</Typography>
         </div>
     )
 }
--- src/components/Login.component.jsx
+++ src/components/Login.component.jsx
@@ -5,6 +5,8 @@ import { useHistory } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles';
 import '../style/Login.style.css'
 
@@ -16,6 +18,14 @@ const useStyles = makeStyles(theme => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    paper: {
+        padding: 20,
+        width: 500,
+        // backgroundColor: "#dcdcdc",
+        //borderColor: theme.secondary,
+        borderRadius: 20,
+        marginTop: 20,
+    }
   }));
 
 function Login(props) {
@@ -57,96 +67,98 @@ function Login(props) {
     const [student, setStudent] = useState(false)
 
     return (
-        <div id="login">
-            <h2>Login</h2>
-            <div className="switch-box" >
-                <div className={`teacher-btn ${!student ? "highlight" : "no-highlight"}`}>Teacher</div>
-                <label className="switch">
-                    <input onClick={() => setStudent(student => !student)} type="checkbox" />
-                    <span className="slider round"></span>
-                </label>
-                <div className={`student-btn ${student ? "highlight" : "no-highlight"}`}>Student</div>
-            </div>
-            {
-                student ?
-                    <React.Fragment>
-                        <form className={classes.form} onSubmit={handleSubmitStudent(onSubmitStudent)}>
-                            <TextField
+        <Grid container justify="center">
+            <Paper className={classes.paper} variant="outlined">
+                <Typography variant="h2" id="login-header">Teleband Login</Typography>
+                <div className="switch-box" >
+                    <div className={`teacher-btn ${!student ? "highlight" : "no-highlight"}`}>Teacher</div>
+                    <label className="switch">
+                        <input onClick={() => setStudent(student => !student)} type="checkbox" />
+                        <span className="slider round"></span>
+                    </label>
+                    <div className={`student-btn ${student ? "highlight" : "no-highlight"}`}>Student</div>
+                </div>
+                {
+                    student ?
+                        <React.Fragment>
+                            <form className={classes.form} onSubmit={handleSubmitStudent(onSubmitStudent)}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="school-id"
+                                    label="School Id"
+                                    name="school-id"
+                                    autoComplete="school-id"
+                                    autoFocus
+                                    type = "text" 
+                                    inputRef={registerStudent({
+                                        required: "Required"
+                                    })}
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submit}
+                                >
+                                    Sign In
+                                </Button>
+                            </form>
+                        </React.Fragment> 
+                        :
+                        <React.Fragment>
+                            <form className={classes.form} onSubmit={handleSubmitTeacher(onSubmitTeacher)}>
+                                <TextField
                                 variant="outlined"
                                 margin="normal"
                                 required
                                 fullWidth
-                                id="school-id"
-                                label="School Id"
-                                name="school-id"
-                                autoComplete="school-id"
+                                id="email"
+                                label="Email"
+                                name="email"
+                                autoComplete="email"
                                 autoFocus
                                 type = "text" 
-                                inputRef={registerStudent({
+                                inputRef={registerTeacher({
+                                    required: "Required",
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                        message: "invalid email address"
+                                    }
+                                })}
+                                />
+                                <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="password"
+                                label="Password"
+                                name="password"
+                                autoComplete="password"
+                                autoFocus
+                                type = "password" 
+                                inputRef={registerTeacher({
                                     required: "Required"
                                 })}
-                            />
-                            <Button
+                                />
+                                <Button
                                 type="submit"
                                 fullWidth
                                 variant="contained"
                                 color="primary"
                                 className={classes.submit}
-                            >
+                                >
                                 Sign In
-                            </Button>
-                        </form>
-                    </React.Fragment> 
-                    :
-                    <React.Fragment>
-                        <form className={classes.form} onSubmit={handleSubmitTeacher(onSubmitTeacher)}>
-                            <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            type = "text" 
-                            inputRef={registerTeacher({
-                                required: "Required",
-                                pattern: {
-                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                    message: "invalid email address"
-                                }
-                            })}
-                            />
-                            <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="password"
-                            label="Password"
-                            name="password"
-                            autoComplete="password"
-                            autoFocus
-                            type = "password" 
-                            inputRef={registerTeacher({
-                                required: "Required"
-                            })}
-                            />
-                            <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            >
-                            Sign In
-                            </Button>
-                        </form>
-                    </React.Fragment>
-            }
-        </div>
+                                </Button>
+                            </form>
+                        </React.Fragment>
+                }
+            </Paper>
+        </Grid>
     )
 }
 
