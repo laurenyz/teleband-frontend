63b8f28977376833ac76f0a4a2061859bf470faa
laurenyz <laurenkyu@gmail.com>
2020-09-13 21:49:59 -0400

    added assignments index/list, headers, and delete functionality to individual assignments

--- src/components/NewAssignmentForm.componenet.jsx
+++ src/components/NewAssignmentForm.componenet.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { FetchURL } from '../env/url'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,24 +21,37 @@ function NewAssignmentForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    fetch('http://localhost:3000/assignments', {
-         method: "POST",
-         headers: {
-             'Content-Type': "application/json",
-             "Accept": "application/json"
-         },
-         body: JSON.stringify({title: title})
-      }).then(resp=>resp.json())
-      .then(json => {
-        setTitle("")
-        console.log(json)
-      })
+    title.length>0?
+      fetch(`${FetchURL}assignments`, {
+          method: "POST",
+          headers: {
+              'Content-Type': "application/json",
+              "Accept": "application/json"
+          },
+          body: JSON.stringify({title})
+        }).then(resp=>resp.json())
+        .then(json => {
+          setTitle("")
+          console.log(json)
+        })
+    : alert("Please enter a title.")
   }
 
   return (
     <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
       <div>
-        <TextField id="standard-name" label="Title" value={title} onChange={handleChange} />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="title"
+        label="Title"
+        type="text"
+        id="title"
+        value={title}
+        onChange = {handleChange} 
+      />
       </div>
         <Button type="submit">Submit</Button>  
     </form>
--- src/containers/AdminPanel.component.jsx
+++ src/containers/AdminPanel.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import AddAssignmentForm from '../components/NewAssignmentForm.componenet'
+import AdminAssignmentsList from '../components/AdminAssignmentsList.component'
+import {Typography} from '@material-ui/core'
 
 function AdminPanel(){
     return(
-        <div>Admin Panel
+        <div>
+            <Typography variant="h4">Add an Assignment</Typography>
             <AddAssignmentForm />
+            <Typography variant="h4">Current Assignments</Typography>
+            <AdminAssignmentsList />
         </div>
     )
 }
