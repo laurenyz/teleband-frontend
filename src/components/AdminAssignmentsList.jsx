c500e445d5732a4b098e3b72e048a6bcdc1679d6
laurenyz <laurenkyu@gmail.com>
2020-09-14 00:00:37 -0400

    added radio buttons to select form type and cleaned up adminAssignmentList

--- src/components/AdminAssignmentsList.jsx
+++ src/components/AdminAssignmentsList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { FetchURL } from '../env/url'
 
 function AdminAssignmentsList({assignments, setAssignments}){
--- src/containers/NewAssignmentContainer.jsx
+++ src/containers/NewAssignmentContainer.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NewAssignmentForm from '../components/NewAssignmentForm'
+import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Grid } from '@material-ui/core'
 
 function NewAssignmentContainer({assignments, setAssignments}){
+    const [formType, setFormType] = useState('audio')
+
+    const displayForm = () => {
+    switch(formType){
+        case 'audio':
+            return <NewAssignmentForm assignments={assignments} setAssignments={setAssignments}/>
+        case 'response':
+            return "response"
+        case 'creative':
+            return "creative"
+        default:
+            return <NewAssignmentForm assignments={assignments} setAssignments={setAssignments}/>
+        }
+    }
+
     return(
         <div>
-            <NewAssignmentForm assignments={assignments} setAssignments={setAssignments}/>
+            <Grid container direction="column" alignItems="center"> 
+                <Grid item>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Assignment Type</FormLabel>
+                        <RadioGroup row aria-label="assignment-type" name="assignment-type" value={formType} onChange={(e)=>setFormType(e.target.value)}>
+                            <FormControlLabel value="audio" control={<Radio />} label="Audio" />
+                            <FormControlLabel value="response" control={<Radio />} label="Response" />
+                            <FormControlLabel value="creative" control={<Radio />} label="Creative" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    {displayForm()}
+                </Grid>
+            </Grid>
+            
         </div>
     )
 }
