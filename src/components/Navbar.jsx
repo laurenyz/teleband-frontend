3a2fc92fdc71dc9ca820140b6c5d983e6bcfedaf
laurenyz <laurenkyu@gmail.com>
2020-10-10 15:36:54 -0400

    updated logos and conditional rendering of login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { AppBar, Grid, Button } from '@material-ui/core/'
+import { AppBar, Button, Grid, Typography } from '@material-ui/core/'
 
 function Navbar({ currentUser, clearUserStates, currentUserType }) {
     const history = useHistory();
@@ -16,38 +16,47 @@ function Navbar({ currentUser, clearUserStates, currentUserType }) {
     function handleLogin() {
         history.push('/login')
     }
-
+    console.log(history)
     return (
         <AppBar position="static" style={{ height: "60px", padding: "10px" }}>
             <Grid container direction="row" spacing={3} justify="space-between" alignItems="center">
                 <Grid item>
-                    <img alt="" src={require('../assets/images/white_teleband_logo.svg')} style={{height:"2rem"}} />
+                    <Grid container alignItems="center" spacing={1}>
+                        <Grid item>
+                            <img alt="" src={require('../assets/images/white_teleband_logo.svg')} style={{height:"2rem"}} />
+                        </Grid>
+                        <Grid item>
+                            <Typography>Tele.band</Typography>
+                        </Grid>
+                    </Grid> 
                 </Grid>
-                <Grid item>
+                {history.location.pathname ==="/admin-panel"?
+                null
+                :<Grid item>
                     <Grid container>
                         {currentUserType === "student" ?
                             <Grid item>
                                 <Button style={{color:"#ffffff"}} onClick={handleAssignmentLink} >
                                     Assignments
-                            </Button >
+                                </Button >
                             </Grid>
                             : null}
-                        {currentUser ?
-                            <Grid item>
-                                <Button style={{color:"#ffffff"}} onClick={handleLogout} >
-                                    Logout
-                            </Button >
-                            </Grid>
+                            {currentUser ?
+                                <Grid item>
+                                    <Button style={{color:"#ffffff"}} onClick={handleLogout} >
+                                        Logout
+                                    </Button>
+                                </Grid>
                             :
                             <Grid item>
                                 <Button style={{color:"#ffffff"}} onClick={handleLogin} >
                                     Login
-                            </Button >
+                                </Button>
                             </Grid>
                         }
                     </Grid>
                 </Grid>
-
+                }
             </Grid>
         </AppBar>
     )
